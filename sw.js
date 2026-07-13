536ae738533d5e50e7cf131ca06e87e488b53bda
Motion-Rail <alkis.kardasopoulos@Motionrail.co.uk>
2026-07-13 13:31:41 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Minimal offline shell. Bump CACHE to force an update after editing the app.
-const CACHE = "tonetester-v9";
+const CACHE = "tonetester-v10";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest",
                 "./icons/icon-192.png", "./icons/icon-512.png", "./logo.png", "./mark.png"];
 self.addEventListener("install", e => {
